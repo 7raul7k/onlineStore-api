ec75928919c8f001bde2fd9131ef6ddb836468b5
Balanescu Raul <balanescuraul@gmail.com>
2023-03-07 18:07:17 +0200

    add more features

--- src/main/java/ro/myclass/onlineStoreapi/repo/OrderRepo.java
+++ src/main/java/ro/myclass/onlineStoreapi/repo/OrderRepo.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ro.myclass.onlineStoreapi.models.Order;
+import ro.myclass.onlineStoreapi.models.OrderDetail;
 
 import java.util.Date;
 import java.util.List;
@@ -18,8 +19,11 @@ public interface OrderRepo extends JpaRepository<Order,Long> {
    @Query("select o from Order o where o.customer.email = ?1 and o.orderDate = ?2")
    Optional<Order> getOrderByCustomerEmailAndOrderDate(String email, Date order);
 
+   @Query("select o from Order o where o.customer.id = ?1 and o.id = ?2")
+   Optional<Order> getOrderByCustomerIdAndAndId(long customerId, long orderId);
 
-
+   @Query("select o from Order o where o.id = ?1")
+   Optional<Order> getOrderById(long id);
 
 
 
